ad47dc8ae54b8eb08aa8c4edf2a1379c3bad8827
Andrew Capp <acapp909@gmail.com>
2020-01-13 00:32:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @word = params[:user_phrase]
+    @pig_latin = PigLatinizer.new.piglatinize(@word)
+    erb :show_result
+  end
 end
